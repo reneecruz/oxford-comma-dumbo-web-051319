50a06c26c6bcd492fabc7a266a396368d81303d6
Renée Cruz <reneebasscruz@gmail.com>
2019-05-06 23:51:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    return array.join(" and ")
+  elsif array.length == 3
+    return array.join(", ")
+  elsif array.length >= 4
+    array.join(",")
+    array << ("and")
+  else 
+    return array.join
+  end
 end
